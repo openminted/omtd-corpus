4dba95ff1c373a37624bde98602a2d9e6b6cc3ff
ilsp <galanisd@gmail.com>
2018-09-10 14:01:58 +0300

    * First impl.

---
+++ src/main/java/eu/openminted/corpus/OMTDCorpus.java
@@ -0,0 +1,10 @@
+package eu.openminted.corpus;
+
+public class OMTDCorpus {
+
+	public final static String subArchiveFullText = "fulltext";
+	public final static String subArchiveAbstracts = "abstract";
+	public final static String subArchiveAnnotations = "annotations";
+	public final static String subArchiveOutput = "output";	
+	
+}
